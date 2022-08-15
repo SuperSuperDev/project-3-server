582b624c73f2befa1325a66a866ab505fe5f2b78
SuperSuperStore <steven@supersuperstore.com>
2022-08-15 15:01:04 +0100

    Updated env and added ARG ENV CLIENT_URL to DockerFile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:16.13.0-alpine3.14
 
+ARG CLIENT_URL
+ENV CLIENT_URL $CLIENT_URL
+
 WORKDIR /usr/app
 COPY package*.json ./
 # RUN npm install -qyg nodemon@2.0.7
--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import cors from 'cors'
 import logger from './middleware/logger.js'
 import router from './views/router.js'
 import errorHandler from './middleware/errorHandler.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000'
 
 const app = express()
 
 app.use(express.json())
 
 const corsOptions = {
-  origin: 'http://cloudify-client.capt.nonovium.com',
+  origin: clientUrl,
 }
 app.use(cors(corsOptions))
 
